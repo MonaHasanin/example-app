72fb67beadb20a8f03a01b442f82cd9501a38301
MonaGalal <50083192+MonaHasanin@users.noreply.github.com>
2024-02-27 11:54:59 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes(['verify'=>true]);
-//Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
